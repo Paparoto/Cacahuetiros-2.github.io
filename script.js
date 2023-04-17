8b22942ff11c986ecd896d08c32d3809c0e4740c
paul.reman <paul.reman@monlycee.net>
2023-04-17 11:46:56 +0200

    Commit

--- script.js
+++ script.js
@@ -20,19 +20,27 @@ function resetimagevente(nombre_vente){
     element.src = "../images/vente" + nombre_vente + ".png"
 }
 
-// Récupération du fichier XML
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        // Parse le fichier XML
-        var xml = this.responseXML;
+function getPrix(nomProduit) {
+    // Récupération du fichier XML
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Parse le fichier XML
+            var xml = this.responseXML;
 
-        // Récupère le prix du produit "Collier cacahuète en Or" grâce à XPath
-        var price = xml.evaluate("//Sheet1[Nom='Collier cacahuète en Or']/Prix", xml, null, XPathResult.NUMBER_TYPE, null).numberValue;
+            // Récupère le prix du produit correspondant au nom donné grâce à XPath
+            var price = xml.evaluate("//Sheet1[Nom='" + nomProduit + "']/Prix", xml, null, XPathResult.NUMBER_TYPE, null).numberValue;
 
-        // Affiche le prix sur la page
-        document.getElementById("price").innerHTML = price.toFixed(2) + " €";
-    }
-};
-xhttp.open("GET", "données.xml", true);
-xhttp.send();
+            // Vérifie que le prix a été trouvé
+            if (price !== null) {
+                // Affiche le prix sur la page
+                console.log("Le prix du produit '" + nomProduit + "' est de " + price.toFixed(2) + " €");
+            } else {
+                console.log("Le produit '" + nomProduit + "' n'a pas été trouvé dans le fichier XML");
+            }
+        }
+    };
+    xhttp.open("GET", "donnees.xml", true);
+    xhttp.send();
+}
+getPrix("Cacahuète en Argent");
